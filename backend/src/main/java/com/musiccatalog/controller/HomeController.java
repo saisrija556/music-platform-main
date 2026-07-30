0addef22e779d2ed355e4308842b78773fb8855f
saisrija556 <saisrija556@gmail.com>
2026-07-30 22:51:44 +0530

    Add HomeController to handle root requests

---
+++ backend/src/main/java/com/musiccatalog/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.musiccatalog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Music Catalog Backend is running!";
+    }
+}
